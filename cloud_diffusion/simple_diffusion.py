10c737a0fe6c27b0695d1341853781dbfd9cb3a7
Thomas Capelle <thomas.capelle@steady-sun.com>
2023-03-14 15:36:43 +0000

    Simple Diffusion training script

--- cloud_diffusion/simple_diffusion.py
+++ cloud_diffusion/simple_diffusion.py
@@ -1,10 +1,19 @@
-import torch
-from torch import nn
+from functools import partial
+
+import torch, math
+from torch import nn, sqrt
+from torch.special import expm1
 from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
 
+from fastprogress import progress_bar
 
-from denoising_diffusion_pytorch.simple_diffusion import UViT, right_pad_dims_to, logsnr_schedule_cosine
+from einops import repeat
+
+try:
+    from denoising_diffusion_pytorch.simple_diffusion import UViT, right_pad_dims_to, logsnr_schedule_cosine
+except:
+    raise ImportError("Please install denoising_diffusion_pytorch with `pip install denoising_diffusion_pytorch`")
 
 def q_sample(x_start, times, noise):
     log_snr = logsnr_schedule_cosine(times)
@@ -38,17 +47,104 @@ def collate_simple_diffusion(b):
     "Collate function that noisifies the last frame"
     return noisify(default_collate(b))
 
-def dl_ddpm(dataset, shuffle=True): 
-    "Create a PyTorch DataLoader that noisifies the last frame"
-    return DataLoader(dataset, 
-                      batch_size=config.batch_size, 
-                      collate_fn=collate_ddpm, 
-                      shuffle=shuffle, 
-                      num_workers=config.num_workers)
-
-uvit = UViT(512, 
+def get_uvit_params(model_name="uvit_small", num_frames=4):
+    "Return the parameters for the diffusers UViT model"
+    if model_name == "uvit_small":
+        return dict(
+            dim=512,
             ff_mult=2,
             vit_depth=4,
             channels=4, 
             patch_size=4,
-            final_img_itransform=nn.Conv2d(4,1,1))
+            final_img_itransform=nn.Conv2d(num_frames,1,1)
+            )
+    elif model_name == "uvit_big":
+        return dict(
+            dim=1024,
+            ff_mult=4,
+            vit_depth=8,
+            channels=4, 
+            patch_size=4,
+            final_img_itransform=nn.Conv2d(num_frames,1,1)
+            )
+    else:
+        raise(f"Model name not found: {model_name}, choose between 'uvit_small' or 'uvit_big'")
+    
+
+
+## Sampling functions
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
+
+
+def logsnr_schedule_cosine(t, logsnr_min = -15, logsnr_max = 15):
+    t_min = math.atan(math.exp(-0.5 * logsnr_max))
+    t_max = math.atan(math.exp(-0.5 * logsnr_min))
+    return -2 * log(torch.tan(t_min + t * (t_max - t_min)))
+
+
+@torch.no_grad()
+def forward(model, past_frames, x, t):
+    return model(torch.cat([past_frames, x], dim=1), t)
+
+
+def p_mean_variance(model, past_frames, x, time, time_next, pred_objective="v"):
+    
+    log_snr = logsnr_schedule_cosine(time)
+    log_snr_next = logsnr_schedule_cosine(time_next)
+    c = -expm1(log_snr - log_snr_next)
+
+    squared_alpha, squared_alpha_next = log_snr.sigmoid(), log_snr_next.sigmoid()
+    squared_sigma, squared_sigma_next = (-log_snr).sigmoid(), (-log_snr_next).sigmoid()
+
+    alpha, sigma, alpha_next = map(sqrt, (squared_alpha, squared_sigma, squared_alpha_next))
+
+    batch_log_snr = repeat(log_snr, ' -> b', b = x.shape[0])
+    
+    # forward pass (this is expensive)
+    pred = forward(model, past_frames, x, t=batch_log_snr)
+
+    if pred_objective == 'v':
+        x_start = alpha * x - sigma * pred
+    elif pred_objective == 'eps':
+        x_start = (x - sigma * pred) / alpha
+
+    x_start.clamp_(-1., 1.)
+
+    model_mean = alpha_next * (x * (1 - c) / alpha + c * x_start)
+
+    posterior_variance = squared_sigma_next * c
+
+    return model_mean, posterior_variance
+
+
+def p_sample(model, past_frames, x, time, time_next):
+    batch, *_, device = *x.shape, x.device
+
+    model_mean, model_variance = p_mean_variance(model, past_frames, x = x, time = time, time_next = time_next)
+
+    if time_next == 0:
+        return model_mean
+
+    noise = torch.randn_like(x)
+    return model_mean + sqrt(model_variance) * noise
+
+
+def p_sample_loop(model, past_frames, steps=500):
+    device = past_frames.device
+    new_frame = torch.randn_like(past_frames[:,-1:], dtype=past_frames.dtype, device=device)
+    time_steps = torch.linspace(1., 0., steps + 1, device = device)
+
+    for i in progress_bar(range(steps), total = steps):
+        times = time_steps[i]
+        times_next = time_steps[i + 1]
+        new_frame = p_sample(model, past_frames, new_frame, time=times, time_next=times_next)
+
+    new_frame.clamp_(-1., 1.)
+    return new_frame
+
+
+def simple_diffusion_sampler(steps=500):
+    """Returns a function that samples from the diffusion model using
+    the simple diffusion sampling scheme"""
+    return partial(p_sample_loop, steps=500)
---
+++ train_uvit.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from types import SimpleNamespace
+
+import wandb
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import OneCycleLR
+from diffusers import UNet2DModel
+
+from cloud_diffusion.dataset import download_dataset, CloudDataset
+from cloud_diffusion.utils import (
+    MiniTrainer, set_seed, parse_args)
+from cloud_diffusion.simple_diffusion import UViT, collate_simple_diffusion, get_uvit_params, simple_diffusion_sampler
+
+
+PROJECT_NAME = "ddpm_clouds"
+DATASET_ARTIFACT = 'capecape/gtc/np_dataset:v1'
+
+config = SimpleNamespace(    
+    epochs = 100, # number of epochs
+    model_name="uvit_small", # model name to save
+    strategy="simple_diffusion", # strategy to use [ddpm, simple_diffusion]
+    noise_steps=1000, # number of noise steps on the diffusion process
+    sampler_steps=333, # number of sampler steps on the diffusion process
+    seed = 42, # random seed
+    batch_size = 6, # batch size
+    img_size = 128, # image size
+    device = "cuda", # device
+    num_workers=8, # number of workers for dataloader
+    num_frames=4, # number of frames to use as input
+    lr = 5e-4, # learning rate
+    validation_days=3, # number of days to use for validation
+    n_preds=8, # number of predictions to make 
+    log_every_epoch = 5, # log every n epochs to wandb
+    )
+
+config.model_params = get_uvit_params(config.model_name, config.num_frames)
+
+set_seed(config.seed)
+device = torch.device(config.device)
+
+# downlaod the dataset from the wandb.Artifact
+files = download_dataset(DATASET_ARTIFACT, PROJECT_NAME)
+
+files = files[0:5]
+
+train_ds = CloudDataset(files=files[:-config.validation_days],  
+                        num_frames=config.num_frames, img_size=config.img_size)
+valid_ds = CloudDataset(files=files[-config.validation_days:], 
+                        num_frames=config.num_frames, img_size=config.img_size).suffle()
+
+collate_fn = collate_simple_diffusion
+
+# DDPM dataloaders
+train_dataloader = DataLoader(train_ds, config.batch_size, shuffle=True, 
+                              collate_fn=collate_fn,  num_workers=config.num_workers)
+valid_dataloader = DataLoader(valid_ds, config.batch_size, shuffle=False, 
+                              collate_fn=collate_fn,  num_workers=config.num_workers)
+
+# model setup
+model = UViT(**config.model_params).to(device)
+
+## optim params
+config.total_train_steps = config.epochs * len(train_dataloader)
+optimizer = AdamW(model.parameters(), lr=config.lr, eps=1e-5)
+scheduler = OneCycleLR(optimizer, max_lr=config.lr, total_steps=config.total_train_steps)
+
+# sampler
+sampler = simple_diffusion_sampler(steps=config.sampler_steps)
+
+# Metrics
+loss_func = torch.nn.MSELoss()
+
+trainer = MiniTrainer(train_dataloader, valid_dataloader, model, optimizer, scheduler, 
+                      sampler, device, loss_func)
+
+if __name__=="__main__":
+    parse_args(config)
+    run = wandb.init(project=PROJECT_NAME, config=config, tags=["ddpm", config.model_name])
+    trainer.fit(config)
+    run.finish()
